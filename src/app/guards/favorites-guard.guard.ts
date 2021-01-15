43e8cb5823ef587a3bf6460755bfc20848d72762
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2021-01-15 01:01:08 -0600

    Change the return value of the guard to Observable<boolean>. In addition, add a regular expression to validate the string taken from the sessionStorage to be right on the http claim

--- src/app/guards/favorites-guard.guard.ts
+++ src/app/guards/favorites-guard.guard.ts
@@ -1,19 +1,30 @@
-import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { RickMortyApiServiceService } from 'src/app/services/rick-morty-api-service.service';
 import { FAV_CHARACTERS } from 'src/app/constants/sesionStorage';
+import { CHARACTER_API } from 'src/app/constants/queries';
 import { CHARACTERS } from 'src/app/constants/paths';
+import { map, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private rickMortyService: RickMortyApiServiceService, private router: Router) { }
+
+  private onFailedCallAPI(): Observable<boolean> {
+    sessionStorage.removeItem(FAV_CHARACTERS);
+    this.router.navigateByUrl(CHARACTERS);
+    return of(false);
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-      const areThereFavorites: boolean = Boolean(sessionStorage.getItem(FAV_CHARACTERS));
-      if (!areThereFavorites) { this.router.navigateByUrl(CHARACTERS); }
-      return areThereFavorites;
-    }
+    state: RouterStateSnapshot): Observable<boolean> {
+      // Use a regular expression to replace all the blank characters and zeros for a '*'.
+      const favoritesIds: string = (sessionStorage.getItem(FAV_CHARACTERS) || '*').replace(/ |0/g, '*');
+      return this.rickMortyService.getItemsFromAPIByIds(CHARACTER_API, favoritesIds).
+        pipe( map(characters => true), catchError(error => this.onFailedCallAPI()) );
+     }
 }
